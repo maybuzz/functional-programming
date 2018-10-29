3e136bfa9c8ad3b2e83bf7b72ae6c6d43e7e8cdf
May <may.johansson@hva.nl>
2018-10-29 16:22:06 +0100

    update README + connect to API

---
+++ README.md
@@ -0,0 +1,22 @@
+# Functional-programming
+
+## Project + Git init shit
+Om project goed op te zeeten en goed te pushen naar GitHub volg deze stappen;
+
+1. `npm init`
+  - project + package.json initialiseren
+2. `git init` in bash
+3. **LEGE** repo aanmaken op GitHub
+  - dus zonder README.md en zonder license
+      - want dan krijg je de stappen te zien.
+4. `git add .`
+  - om alle files toe te voegen
+5. `git status`
+  - om te checken wat er klaar staat om te committen
+6. `git commit -m "bericht"`
+  - om te committen met een message
+7. `git remote add origin https://github.com/GITHUB-NAAM/REPOSITORY-NAAM.git`
+  - om vast te stellen waar je het naar moet pushen
+8. `git push -u origin master`
+  - om te pushen naar de master
+9. Vervolgens doe je telkens weer `git add .`, `git status` en `git commit -m "..."` en `git push` om te pushen
--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-const OBA = require('oba-api')
+// const OBA = require('oba-api')
+const axios = require('axios');
+var parseString = require('xml2js').parseString;
 
 require('dotenv').config()
 
-// Setup authentication to api server
-const client = new OBA({
-  // ProQuest API Keys
+// // Setup authentication to api server
+const allKeys = {
   public: process.env.PUBLIC,
   secret: process.env.SECRET
-})
+}
+
+const searchEndpoint = '&pagesize'
+
+axios.get(`https://zoeken.oba.nl/api/v1/search/?q=boek&authorization=${allKeys.public + ''}`)
+  .then(function (response) {
+    // handle success
+    console.log('succes')
+
+    parseString(response.data, function(err, result) {
+      if(err){console.log(err)}
+      else {
+        console.log(result);
+      }
+    })
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
 
 // General usage:
 // client.get({ENDPOINT}, {PARAMS});
 // ENDPOINT = search | details | refine | schema | availability | holdings
 // PARAMS = API url parameter options (see api docs for more info)
-
+//
 // Client returns a promise which resolves the APIs output in JSON
-
+//
 // Example search to the word 'rijk' sorted by title:
-client.get('search', {
-  q: 'rijk',
-  sort: 'title'
-})
-  .then(res => console.log(res)) // JSON results
-  .catch(err => console.log(err)) // Something went wrong in the request to the API
+// client.get('search', {
+//   q: 'rijk',
+//   sort: 'title'
+// })
+//   .then(res => console.log(res)) // JSON results
+//   .catch(err => console.log(err)) // Something went wrong in the request to the API
